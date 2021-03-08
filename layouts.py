4d7ae2ed11ef89c98974147a69bcd1047207d257
Artem Smaznov <31800376+ArtemSmaznov@users.noreply.github.com>
2021-03-08 00:11:52 +0000

    Initial file separation

---
+++ layouts.py
@@ -0,0 +1,41 @@
+from libqtile import layout
+from libqtile.config import Match
+
+layout_theme = {"border_width": 3,
+                "margin": 4,
+                "border_focus_stack": '#d75f5f'
+                }
+
+layouts = [
+    layout.Columns(**layout_theme),
+    layout.MonadTall(**layout_theme),
+    layout.Max(),
+    #  layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    # layout.Matrix(),
+    # layout.MonadWide(),
+    # layout.RatioTile(),
+    #  layout.Tile(**layout_theme),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+]
+
+# Run the utility of `xprop` to see the wm class and name of an X client.
+floating_layout = layout.Floating(float_rules=[
+    *layout.Floating.default_float_rules,
+    Match(wm_class='confirm'),
+    Match(wm_class='dialog'),
+    Match(wm_class='download'),
+    Match(wm_class='error'),
+    Match(wm_class='file_progress'),
+    Match(wm_class='notofication'),
+    Match(wm_class='splash'),
+    Match(wm_class='toolbar'),
+    Match(wm_class='confirmreset'),  # gitk
+    Match(wm_class='makebranch'),  # gitk
+    Match(wm_class='maketag'),  # gitk
+    Match(wm_class='ssh-askpass'),  # ssh-askpass
+    Match(title='branchdialog'),  # gitk
+    Match(title='pinentry'),  # GPG key password entry
+])
