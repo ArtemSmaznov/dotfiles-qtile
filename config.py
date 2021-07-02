c99ca65f90d9681c53d39f014cabe0ff2b52e24f
Artem Smaznov <31800376+ArtemSmaznov@users.noreply.github.com>
2021-07-02 22:54:11 +0100

    Cleaned up config.py

--- config.py
+++ config.py
@@ -12,30 +12,18 @@ from settings.layouts import floating_layout, layouts
 from settings.screens import screens
 
 
-#  dgroups_key_binder = None
-main = None  # WARNING: this is deprecated and will be removed soon
-follow_mouse_focus = False
-bring_front_click = False
-cursor_warp = False
-dgroups_app_rules = []  # type: List
 auto_fullscreen = True
+bring_front_click = "floating_only"
+cursor_warp = False
+#  dgroups_key_binder = None
+dgroups_app_rules = []  # type: List
 focus_on_window_activation = "smart"
+follow_mouse_focus = False
+reconfigure_screens = True
+auto_minimize = True
 
 
-@ hook.subscribe.startup_once
+@hook.subscribe.startup_once
 def autostart():
-    autostart_script = os.path.expanduser(
-        '~/.config/autostart-scripts/autostart.sh'
-    )
+    autostart_script = os.path.expanduser("~/.config/autostart-scripts/autostart.sh")
     subprocess.call([autostart_script])
-
-
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
-wmname = "LG3D"
