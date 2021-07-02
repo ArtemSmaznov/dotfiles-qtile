040912610ab3b4ccd37798bf49988d0d765a2e69
Artem Smaznov <31800376+ArtemSmaznov@users.noreply.github.com>
2021-07-02 23:19:55 +0100

    Refactored groups

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from libqtile.lazy import lazy
 
 from keys.bindings import keys, mouse
 from lib.widgets import widget_defaults, extension_defaults
-from settings.groups import groups
+from settings.groups import groups, dgroups_key_binder
 from settings.layouts import floating_layout, layouts
 from settings.screens import screens
 
@@ -15,7 +15,6 @@ from settings.screens import screens
 auto_fullscreen = True
 bring_front_click = "floating_only"
 cursor_warp = False
-#  dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
 focus_on_window_activation = "smart"
 follow_mouse_focus = False
--- settings/groups.py
+++ settings/groups.py
@@ -1,188 +1,167 @@
 import re
 
 from libqtile.config import DropDown, Group, Key, ScratchPad
-from libqtile.dgroups import Match
+from libqtile.dgroups import Match, simple_key_binder
 from libqtile.lazy import lazy
 
 from keys.bindings import keys
 from keys.mods import mod, shift
 import settings.apps as apps
 
-# Optional group parameters
-# "label": "",
-# "layout": "columns",
-# 'spawn': apps.web_browser,
-
 # Most icons taken from https://fontawesome.com/
-group_names = [
-    (
-        "internet",
-        {
-            "label": "",
-            "matches": [
-                Match(
-                    wm_class=[
-                        "firefox",
-                        "Tor Browser",
-                        "Chromium",
-                        "Google-chrome",
-                        "Brave-browser",
-                        "vivaldi-stable",
-                    ]
-                ),
-            ],
-        },
-    ),
-    (
-        "gaming",
-        {
-            "label": "",
-            "matches": [
-                Match(
-                    wm_class=[
-                        "Wine",
-                        "dolphin-emu",
-                        "Lutris",
-                        "Citra",
-                        "SuperTuxKart",
-                        "Steam",
-                    ]
-                ),
-                Match(title="Steam"),
-            ],
-        },
-    ),
-    (
-        "coding",
-        {
-            "label": "",
-            "spawn": apps.terminal,
-            "matches": [
-                Match(
-                    wm_class=[
-                        # 'Alacritty',
-                        # 'Terminator',
-                        # 'URxvt',
-                        # 'UXTerm',
-                        # 'kitty',
-                        # 'K3rmit',
-                        # 'XTerm',
-                        "Geany",
-                        "Atom",
-                        "Subl3",
-                        "code-oss",
-                        "Emacs",
-                        "Cypress",
-                        "Oomox",
-                        "Unity",
-                        "UnityHub",
-                        "jetbrains-studio",
-                    ]
-                ),
-            ],
-        },
-    ),
-    (
-        "computer",
-        {
-            "label": "",
-            "matches": [
-                Match(
-                    wm_class=[
-                        "dolphin",
-                        "ark",
-                        "Nemo",
-                        "File-roller",
-                        "googledocs",
-                        "keep",
-                        "calendar",
-                    ]
-                ),
-            ],
-        },
-    ),
-    (
-        "music",
-        {
-            "label": "",
-            "spawn": apps.cli_music_player,
-            "matches": [
-                Match(
-                    wm_class=[
-                        "Spotify",
-                        "youtubemusic-nativefier-040164",
-                    ]
-                ),
-            ],
-        },
-    ),
-    (
-        "graphics",
-        {
-            "label": "",
-            "matches": [
-                Match(
-                    wm_class=[
-                        "Gimp-2.10",
-                        "Gimp",
-                        "Inkscape",
-                        "Flowblade",
-                        "digikam",
-                    ]
-                ),
-            ],
-        },
-    ),
-    (
-        "video",
-        {
-            "label": "",
-            "matches": [
-                Match(
-                    wm_class=[
-                        "vlc",
-                        "Celluloid",
-                        "obs",
-                        "kdenlive",
-                    ]
-                ),
-            ],
-        },
-    ),
-    (
-        "chat",
-        {
-            "label": "",
-            "matches": [
-                Match(
-                    wm_class=[
-                        "whatsapp-for-linux",
-                        "Slack",
-                        "discord",
-                        "signal",
-                    ]
-                ),
-            ],
-        },
-    ),
-    (
-        "sandbox",
-        {
-            "label": "",
-            "matches": [
-                Match(
-                    wm_class=[
-                        "VirtualBox Manage",
-                        "VirtualBox Machine",
-                    ]
-                ),
-            ],
-        },
-    ),
-]
+
+# Optional group parameters
+# label="",
+# layout="columns",
+# spawn=apps.web_browser,
 
 groups = [
-    *[Group(name, **kwargs) for name, kwargs in group_names],
+    Group(
+        "internet",
+        label="",
+        matches=[
+            Match(
+                wm_class=[
+                    "firefox",
+                    "Tor Browser",
+                    "Chromium",
+                    "Google-chrome",
+                    "Brave-browser",
+                    "vivaldi-stable",
+                ]
+            ),
+        ],
+    ),
+    Group(
+        "gaming",
+        label="",
+        matches=[
+            Match(
+                wm_class=[
+                    "Wine",
+                    "dolphin-emu",
+                    "Lutris",
+                    "Citra",
+                    "SuperTuxKart",
+                    "Steam",
+                ]
+            ),
+            Match(title="Steam"),
+        ],
+    ),
+    Group(
+        "coding",
+        label="",
+        spawn=apps.terminal,
+        matches=[
+            Match(
+                wm_class=[
+                    # 'Alacritty',
+                    # 'Terminator',
+                    # 'URxvt',
+                    # 'UXTerm',
+                    # 'kitty',
+                    # 'K3rmit',
+                    # 'XTerm',
+                    "Geany",
+                    "Atom",
+                    "Subl3",
+                    "code-oss",
+                    "Emacs",
+                    "Cypress",
+                    "Oomox",
+                    "Unity",
+                    "UnityHub",
+                    "jetbrains-studio",
+                ]
+            ),
+        ],
+    ),
+    Group(
+        "computer",
+        label="",
+        matches=[
+            Match(
+                wm_class=[
+                    "dolphin",
+                    "ark",
+                    "Nemo",
+                    "File-roller",
+                    "googledocs",
+                    "keep",
+                    "calendar",
+                ]
+            ),
+        ],
+    ),
+    Group(
+        "music",
+        label="",
+        spawn=apps.cli_music_player,
+        matches=[
+            Match(
+                wm_class=[
+                    "Spotify",
+                    "youtubemusic-nativefier-040164",
+                ]
+            ),
+        ],
+    ),
+    Group(
+        "graphics",
+        label="",
+        matches=[
+            Match(
+                wm_class=[
+                    "Gimp-2.10",
+                    "Gimp",
+                    "Inkscape",
+                    "Flowblade",
+                    "digikam",
+                ]
+            ),
+        ],
+    ),
+    Group(
+        "video",
+        label="",
+        matches=[
+            Match(
+                wm_class=[
+                    "vlc",
+                    "Celluloid",
+                    "obs",
+                    "kdenlive",
+                ]
+            ),
+        ],
+    ),
+    Group(
+        "chat",
+        label="",
+        matches=[
+            Match(
+                wm_class=[
+                    "whatsapp-for-linux",
+                    "Slack",
+                    "discord",
+                    "signal",
+                ]
+            ),
+        ],
+    ),
+    Group(
+        "sandbox",
+        label="",
+        matches=[
+            Match(
+                wm_class=[
+                    "VirtualBox Manage",
+                    "VirtualBox Machine",
+                ]
+            ),
+        ],
+    ),
     ScratchPad(
         "scratchpad",
         [
@@ -191,11 +170,6 @@ groups = [
     ),
 ]
 
-for i, (name, kwargs) in enumerate(group_names, 1):
-    # Switch to another group with SUPER + #
-    keys.append(Key([mod], str(i), lazy.group[name].toscreen()))
-
-    # Send current window to another group SUPER + SHIFT + #
-    keys.append(
-        Key([mod, shift], str(i), lazy.window.togroup(name, switch_group=False))
-    )
+# Switch to another group with SUPER + #
+# Send current window to another group SUPER + SHIFT + #
+dgroups_key_binder = simple_key_binder(mod)
